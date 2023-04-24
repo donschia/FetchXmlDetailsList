76088965dcaa41334fb2d3270e5b11213d11691e
Don Schiavone <don.schiavone@gmail.com>
2023-04-23 23:48:14 -0500

    Documentation

--- README.md
+++ README.md
@@ -1,63 +1,111 @@
-# FetchXmlDetailsList
-Open VS Code to this folder.
-Open Terminal.
+# FetchXml DetailsList
+## Description
+PCF Control for showing subgrids from FetchXml queries.  The query can be a complex as needed with many linked entities.  This will allow getting around the limits of most Model Driven App views.  You will include an ID Placeholder to replace with the current record id.
+[SCREENSHOT]
 
-To install dependencies run:
-	npm install
+Also you need to provide a columnLayout json to define all column details. 
+## Features
+- Dynamic queries can be more complex than views allow.  
+- Uses Placeholder to filter by current record id.
+- Look and Feel is similar to out of the box Model Driven App read only subgrid.  Supports sorting and resizing of columns.
+- Quick rendering.
+- Double clicking the row navigates to the base record.
+- Support navigating to linked entities.
+- Customization options for each column include date formatting, toggleable entity linking, absolute urls, relative urls, and so on.
+- Debug mode to see examine data returned from FetchXml query for building column layout.
+  
+***
+## Initial Setup
+1. Open VS Code to this folder.
+2. Open Terminal (Control + Shift + ~)
+3. Run <code>npm install</code>
+   
+## Build and Deploy
+Simply run <code> npm start</code> to launch the FetchXmlDetailsList in the PCF Test Harness.  This loads sample data along with a sample columnLayout.  Linking is disabled since this is not allowed in the tester.
 
-To build and deploy to your currently configured DEV Environment using PAC PCF PUSH run:
-	buildAndDeploy.ps1
-
-This will build the component, add it to a temporary solution (PowerAppsTools_onegas) , import to the DEV environment and publish all.
+You can build and deploy to your currently configured DEVELOPMENT Environment using the CLI [PAC PCF PUSH](https://learn.microsoft.com/en-us/power-platform/developer/cli/reference/pcf#pac-pcf-push) by running:  <code>buildAndDeploy.ps1</code>.  Note that the CLI requires connecting to your development org first. See the documentation for more details.
+You will need to ensure you have installed the [Microsoft PowerApps CLI](https://learn.microsoft.com/en-us/power-platform/developer/cli/introduction#install-power-apps-cli). 
+   - <code>buildAndDeploy.ps1</code> will build the component, add it to a temporary solution (PowerAppsTools_YourOrg) , import to your DEV environment and Publish All.
 Prerequitiste is to make sure you can connect to your DEV environment using the CLI tools.
 
+## Input Parameters (Properties)
 The grid has input parameters which must be set.
 
-FetchXML is the full FetchXml with a placeholder for the Record Id in place.
+- <code>FetchXml</code> is the full FetchXml with a placeholder for the Record Id in place.
 
-RecordIdPlaceholder is the placeholder.  
-I.e. [RECORDID]
+- <code>RecordIdPlaceholder</code> is the placeholder text. This will be replaced with the current record id.  
+i.e. <code>[RECORDID]</code>
 
-RecordId is read from the parent in a bit of a hack at the moment as it's not super easy to get this in the Power Apps framework.
+- The <code>RecordId</code> is read from the parent in a bit of a hack at the moment as it's not super easy to get this in the Power Apps framework.
 
-ColumnLayoutJson is a collection of columns used for the table layout.
+- <code>ColumnLayoutJson</code> is a collection of columns used for the table layout.
+- <code>ItemsPerPage</code> is current defaulted to 5000 as paging is currently not implemented.  // [NOT SUPPORTED CURRENTLY] ItemsPerPage is how many items to show per page. For now this is set at 5000 since paging and sorting seem to be at odds with eachother.
+- <code>DebugMode</code> can be set to <code>On</code> or <code>Off</code>.  When enabled, this will write extra details to console, break when entering the main control, and break on handled exceptions.
 
-// [NOT SUPPORTED CURRENTLY] ItemsPerPage is how many items to show per page.  For now this is set at 5000 since paging and sorting seem to be at odds with eachother.
+## Set Up Note
+Both the new and legacy designers will likely not allow you to paste in text long enough to be useful so you have to use a [workaround to extend the field length](https://powerusers.microsoft.com/t5/Power-Apps-Pro-Dev-ISV/Problem-with-maximum-length-of-Input-parameters-which-are-of/td-p/288295).
 
-SETUP NOTE:
-Both the new and legacy designers will not allow you to paste in text long enough to be useful so you have to use a workaround.
-https://powerusers.microsoft.com/t5/Power-Apps-Pro-Dev-ISV/Problem-with-maximum-length-of-Input-parameters-which-are-of/td-p/288295
+Essentially you use the legacy designer and hack the input box via F11 dev tools to set the maxlength to something like 9999 instead of the default 2000 if your text doesn't fit.
 
-Essentially you need to use the legacy designer and hack the input box via F11 dev tools set the maxlength to something like 9999 instead of the default 2000 if your text doesn't fit.
+## ColumnLayoutJson
+This is a list of [IColumn](https://learn.microsoft.com/en-us/javascript/api/sp-listview-extensibility/icolumn?view=sp-typescript-latest) from the [FluentUI DetailsList]( https://developer.microsoft.com/en-us/fluentui#/controls/web/detailslist).  Simply include all of the required fields for each column your data grid.  The options <code>data</code> object can be helpful for extra customization.
 
------
-Issues:
-There seems to be an issue with the dynamics-web-api 3rd party library.  This gives a strange crypto error.  I was able to fix it by hacking the webpackConfig for pcf-scripts to tell it to ignore it.
-https://github.com/donschia/FetchXmlDetailsList/blob/master/node_modules/pcf-scripts/webpackConfig.js#L61
+
+| Field Name | Required | Type | Description |
+| --- | --- | -- | -- |
+| key | Yes | String |  Unique key for data item |
+| fieldName | Yes |  String |  Column Label |
+| name | Yes |  String |  Field name matched from the returned Xrm Data |
+| minWidth | Yes |  Number |  Minimum field width (ie. 50) |
+| data | No | Object| Data Object with special stuff.  See defination below. |
+
+### data Object
+| Field Name | Required | Type | Description |
+| --- | --- | -- | -- |
+|dateFormat | No | String | You can force a date into a particular format by specifying this.  This uses [date-fns placeholders](https://date-fns.org/v2.29.3/docs/format) i.e. "yyyy-MM-dd" |
+|entityLinking | No | Boolean | Set to False to prevent linkign to linked entities. Otherwise links are enabled. |
+|url | No | String | Absolute URL.  Or can be relative from the [BASED365URL] path. |
+
+Sample:
+<code>
+"key": "banneraccount.onegas_contractid",
+    "fieldName": "banneraccount.onegas_contractid_Formatted",
+    "name": "Contract Name(BA)",
+    "minWidth": 100,
+    "data": {
+      "url": "[BASED365URL]/main.aspx?etc=1010&pagetype=entityrecord&id=[ID]"
+    }
+</code>
+
+# Issues
+There seems to be an issue with the dynamics-web-api 3rd party library. This gives a strange crypto error. I was able to fix it by hacking the webpackConfig for pcf-scripts to tell it to ignore it.
+../FetchXmlDetailsList/blob/master/node_modules/pcf-scripts/webpackConfig.js#L61
 
 Change this:
- resolve: {
-            // Tell webpack which extensions to try when it is looking for a file.
-            extensions: ['.ts', '.tsx', '.js', '.jsx'],
-        },
+```javascript
+resolve: {
+        // Tell webpack which extensions to try when it is looking for a file.
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+},
+
+```
 
 to this:
- resolve: {
-            // Tell webpack which extensions to try when it is looking for a file.
-            extensions: ['.ts', '.tsx', '.js', '.jsx'],
-            fallback: {
-				"crypto": false
-			},
-        },
+```javascript
+resolve: {
+        // Tell webpack which extensions to try when it is looking for a file.
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        fallback: { "crypto": false },
+},
+```
+---
 
-
-------------
-
-Errors:
-If you get this following runtime error you may need to use a _Formatted field.  For example, here it seems to be having a hard time with the date. 
+## Errors
+If you get this following runtime error you may need to use a \_Formatted field. For example, here it seems to be having a hard time with the date.
 Objects are not valid as a React child (found: Wed Dec 31 9000 00:00:00 GMT-0600 (Central Standard Time)). If you meant to render a collection of children, use an array instead.
 Another option to format the output.
 
-TODO:
-No paging support currently.  Page size is locked at 5000 for now.
+***
 
+## TODOs:
+No paging support currently. Page size is locked at 5000 for now.
--- src/DynamicDetailsList.tsx
+++ src/DynamicDetailsList.tsx
@@ -29,7 +29,6 @@ export interface IDynamicDetailsListProps {
     baseD365Url?: string;
 }
 
-
 export interface IDynamicDetailsListState {
     columns: IColumn[];
     items: any[]; //Record<string, string>[] //any[];
@@ -39,24 +38,17 @@ export interface IDynamicDetailsListState {
     baseD365Url?: string;
 }
 
-
 export class DynamicDetailsList extends React.Component<any, IDynamicDetailsListState> {
     private _allItems: any[];
     private _columns: any;
     private _pcfContext: any;
     private _webApi: DynamicsWebApi;
-
-    // private _selection: Selection;
     private _primaryEntityName: string;
     private _fetchXml: string;
     private _announcedMessage: string;
     private _currentPageNumber: number;
     private _isDebugMode: boolean;
     private _baseD365Url?: string;
-
-    //private _fetchXmlIdPlaceholder: string;
-    //private _recordId: string;
-
 
     constructor(props: any) {
         super(props);
@@ -68,41 +60,32 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
         this._pcfContext = props.context;
         this._primaryEntityName = props.primaryEntityName;
         this._fetchXml = props.fetchXml;
-        //this._fetchXmlIdPlaceholder = props.fetchXmlIdPlaceholder;
-        //this._recordId = props.recordId;
         this._allItems = props.items;
         this._columns = props.columns;
         this._isDebugMode = props.isDebugMode;
         this._baseD365Url = props.baseD365Url;
-
         this._currentPageNumber = 1;
-
         let useDynamicsWebApi: boolean = true;
 
-        // Do we actually have a query to run, or are we using the sample data
+        // Check we actually have a query to run, otherwise try to use sample data
         // Don't bother with Web Api if we aren't actually fetching data
-        // This way we can quickly see changes in the PCF Test harness and not have to burn a full deploy cycle
+        // This way we can quickly see changes in the PCF Test harness and not have to endure a full deploy cycle
+        // Test harness will not have FetchXml input varaible available when debugging in test harness
         if (props.fetchXml) {
 
+            // columnLayout
+            // columnLayout with click handler added
             this._columns = this._columns.map((column: IColumn) => ({
-                key: column.key,
-                name: column.name,
-                ariaLabel: column.name,
-                fieldName: column.fieldName,
-                minWidth: column.minWidth,
-                flexGrow: 1,
-                data: column.data,
-                //maxWidth: 200,
-                isResizable: true,
-                onColumnClick: this._onColumnClick
+                ...column,
+                onColumnClick: this._onColumnClick,
             }));
 
             if (this._isDebugMode) {
                 console.log(`useDynamicsWebApi: ${useDynamicsWebApi}`);
             }
 
+            // 3rd party DynamicsWebApi library allows access to the _Formatted items
             if (useDynamicsWebApi) {
-                // 3rd party DynamicsWebApi library allows access to the _Formatted items
                 this._webApi = new DynamicsWebApi(
                     {
                         dataApi: { version: '9.0' },
@@ -111,7 +94,6 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
 
                 this._webApi.executeFetchXml(this._primaryEntityName, this._fetchXml, "*", this._currentPageNumber, undefined, undefined)
                     .then((data) => {
-                        //debugger;
                         // this._pagingCookie = data["@Microsoft.Dynamics.CRM.fetchxmlpagingcookie"];
                         //this._totalNumberOfRecords =  data.count;
                         // Sometimes data is an array[]                
@@ -131,7 +113,6 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                                 }
                             );
                         }
-
                     }).catch((e) => {
                         if (this._isDebugMode) {
                             console.log(e);
@@ -141,10 +122,9 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                             announcedMessage: "Error fetching records"
                         });
                     });
-
             }
             else {
-                // just use regular out of the box (lacks _Formatted helpers)
+                // or just use regular out of the box (but lacks _Formatted helpers)
                 this._pcfContext.webAPI.retrieveMultipleRecords(this._primaryEntityName, "?fetchXml=" + encodeURIComponent(this._fetchXml)).then(
                     (results: any) => {
                         if (results && results.entities && results.entities.length > 0) {
@@ -173,55 +153,53 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                     });
             }
         }
-        // If we don't have any data, use sample data
+        // If we don't have a query or any data, use sample data
         else if (!props.fetchXml || !props.dataItems || props.dataItems.length < 1) {
             var sampleData = GetSampleData();
+
+            if (this._isDebugMode) {
+                console.log(`Sample Data`, sampleData);
+            }
+
             if (sampleData.dataItems && sampleData.dataItems.length > 0) {
                 this._allItems = sampleData.dataItems;
                 this._columns = sampleData.columns;
                 this._primaryEntityName = sampleData.primaryEntityName;
 
+                // columnLayout with click handler added
                 this._columns = this._columns.map((column: IColumn) => ({
-                    key: column.key,
-                    name: column.name,
-                    ariaLabel: column.name,
-                    fieldName: column.fieldName,
-                    minWidth: column.minWidth,
-                    flexGrow: 1,
-                    //maxWidth: 200,
-                    isResizable: true,
-                    data: column.data,
-                    // Sorting
+                    ...column,
                     onColumnClick: this._onColumnClick,
-                    // Handle rendering lookup field links
-                    /*
-                    onRender: (item: any) => {
-                        //if (column.key.indexOf(_LOOKUPLOGICALNAMEATTRIBUTE) > -1) {
-                        if (item[column.key]) {
-                            if (item[column.key + _LOOKUPLOGICALNAMEATTRIBUTE]) {
-                                //let baseFieldName = column.key.replace(_LOOKUPLOGICALNAMEATTRIBUTE, "");
-                                <Link key={item} onClick={() => this._pcfContext.navigation.openForm({ entityName: item[column.key + _LOOKUPLOGICALNAMEATTRIBUTE], entityId: item[column.key] })}>
-                                    Link: {item[column.key]}
-                                </Link>
-                            }
-                            else {
-                                <span>{item[column.key]}</span>
-                            }
-                        }
-
-                    },
-                    */
                 }));
+                /*
+                 // Or if you want more control over which IColumn properties are supported
+                 this._columns = this._columns.map((column: IColumn) => ({
+                     key: column.key,
+                     name: column.name,
+                     ariaLabel: column.name,
+                     fieldName: column.fieldName,
+                     minWidth: column.minWidth,
+                     flexGrow: 1,
+                     //maxWidth: 200,
+                     isResizable: true,
+                     data: column.data,
+                     // Sorting
+                     onColumnClick: this._onColumnClick,
+                     // Handle rendering lookup field links
+                 }));
+                 */
 
                 this._announcedMessage = "Using sample data...";
                 this.setState({
-                    //this.state = {
                     items: this._allItems,
                     columns: this._columns,
                     fetchXml: this._fetchXml,
                     announcedMessage: this._announcedMessage
                 });
             }
+        }
+        else {
+            this._announcedMessage = "Cannot connect via Xrm and no sample data found.";
         }
 
         this.state = {
@@ -235,8 +213,9 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
     public componentDidUpdate(previousProps: any, previousState: IDynamicDetailsListProps) {
     }
 
-    // DetailsList documentation here:
+    // FluentUI DetailsList documentation here:
     // https://developer.microsoft.com/en-us/fluentui#/controls/web/detailslist
+    // ScollablePane is helpful to recreate the standard Model-driven app experience
     public render(): JSX.Element {
         const { columns, items, announcedMessage } = this.state;
         // if (this._isDebugMode) { console.log(items); }
@@ -261,13 +240,11 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                                     constrainMode={ConstrainMode.unconstrained}
                                     onRenderRow={this._onRenderRow}
                                     onRenderDetailsHeader={this._onRenderDetailsHeader}
-                                    // Double clicking a row
-                                    //onItemInvoked={this._onItemInvoked}
-                                    // Custom Rendering to support entity linking
+                                    // Custom Rendering to support entity linking, Absolute Urls, formatted dates, etc.
                                     onRenderItemColumn={this._renderItemColumn}
-
+                                    // Double clicking a row opens the record in Model Driven App
+                                    // Test harness will give error "Your control is trying to open a form. This is not yet supported."
                                     onItemInvoked={(item: any) => {
-                                        // debugger;  // eslint-disable-line no-debugger
                                         this._pcfContext.navigation.openForm({
                                             entityName: this._primaryEntityName,
                                             entityId: item[this._primaryEntityName + "id"]
@@ -281,7 +258,6 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                         </ScrollablePane>
                     </Stack >
                 </>
-
             );
         }
         else
@@ -292,12 +268,12 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
             );
     }
 
-    //private _onColumnClick = (ev: React.MouseEvent<HTMLElement>, column: IColumn): void => {
-    // _renderItemColumn(item: any, index: number | undefined, column: any) {
     private _renderItemColumn = (item: any, index: number | undefined, column: any): any => {
         // debugger; // eslint-disable-line no-debugger
         //const fieldContent = item[column.fieldName as keyof any] as string;
         let fieldContent = item[column.fieldName];
+
+        console.log(fieldContent, column, column.data);
 
         if (item[column.key]) {
             // Handle any custom Date Formats via date=fns format string
@@ -310,23 +286,33 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
                     return (<span>{format(dateValue, column.data.dateFormat)}</span>);
                 }
                 catch (ex) {
-                    // ignore
+                    if (this._isDebugMode) {
+                        console.log(ex);
+                        debugger; // eslint-disable-line no-debugger
+                    }
+                    // ignore error and just render as-is
                     return (<span>{fieldContent}</span>);
                 }
             }
 
-            if (column.data && column.data.url && column.data.url !== "") {
+            // URL Handling
+            // URL field handling - opens in a new tab/window
+            //else if (fieldContent && String(fieldContent).startsWith("http")) {
+            if (column.data && column.data.url == _USE_VALUE_URL_PLACEHOLDER) {
+                let linkText = (column.data.urlLinkText && column.data.urlLinkText == _USE_VALUE_URL_PLACEHOLDER) ? fieldContent :
+                    column.data.urlLinkText && column.data.urlLinkTextfieldContent != "" ? column.data.urlLinkText :
+                        "External Link";
+                return (<Link key={item} href={fieldContent} target="_blank">{linkText}</Link>);
+            }
+            // Absolute URL Handling (with or without placeholders)
+            else if (column.data && column.data.url && column.data.url !== "") {
                 let url = column.data.url
                     .replace(_BASE_D365_URL_PLACEHOLDER, this._baseD365Url)
                     .replace(_RECORD_ID_URL_PLACEHOLDER, item[column.key]);
                 return (<Link key={item} href={url} target="_blank">{fieldContent}</Link>);
             }
-            // URL field handling - open in a new tab/window
-            //else if (fieldContent && String(fieldContent).startsWith("http")) {
-            else if (column.data && column.data.url === _USE_VALUE_URL_PLACEHOLDER) {
-                return (<Link key={item} href={fieldContent} target="_blank">External Link</Link>);
-            }
-            // Support linking to other entities
+            // Support navigation to entity links
+            // Test harness will give error "Your control is trying to open a form. This is not yet supported."
             else if (item[column.key + _LOOKUPLOGICALNAMEATTRIBUTE]) {
                 if (column.data && column.data.entityLinking && column.data.entityLinking == true) {
                     //let baseFieldName = column.key.replace(_LOOKUPLOGICALNAMEATTRIBUTE, "");
@@ -345,31 +331,9 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
         }
     }
 
-    private _onItemInvoked(item: any): void {
-        // Open the form.
-        this._pcfContext.navigation.openForm(
-            {
-                entityName: this._primaryEntityName,
-                entityId: item[this._primaryEntityName + "id"]
-            }
-        );
-    }
-
-    private link_Click(evt: Event): void {
-        var currentItem = evt.currentTarget;
-
-        // @ts-ignore
-        var data = currentItem.dataset;
-        var recordId = data.recordId;
-        var recordLogicalName = data.recordLogicalName;
-
-        var entityFormOptions: { entityName?: string, entityId?: string, openInNewWindow?: boolean } = {};
-        entityFormOptions.entityName = recordLogicalName;
-        entityFormOptions.entityId = recordId;
-        //entityFormOptions.openInNewWindow = window.event.ctrlKey;
-    }
 
     private _onColumnClick = (ev: React.MouseEvent<HTMLElement>, column: IColumn): void => {
+        // Handle sorting
         const { columns, items } = this.state;
         const newColumns: IColumn[] = columns.slice();
         const currColumn: IColumn = newColumns.filter(currCol => column.key === currCol.key)[0];
@@ -424,4 +388,32 @@ export class DynamicDetailsList extends React.Component<any, IDynamicDetailsList
         }
         return null;
     };
+
+    // TODO: REMOVED UNUSED
+    // UNUSED 
+    private _onItemInvoked(item: any): void {
+        // Open the form
+        this._pcfContext.navigation.openForm(
+            {
+                entityName: this._primaryEntityName,
+                entityId: item[this._primaryEntityName + "id"]
+            }
+        );
+    }
+
+    // UNUSED
+    private link_Click(evt: Event): void {
+        var currentItem = evt.currentTarget;
+
+        // @ts-ignore
+        var data = currentItem.dataset;
+        var recordId = data.recordId;
+        var recordLogicalName = data.recordLogicalName;
+
+        var entityFormOptions: { entityName?: string, entityId?: string, openInNewWindow?: boolean } = {};
+        entityFormOptions.entityName = recordLogicalName;
+        entityFormOptions.entityId = recordId;
+        //entityFormOptions.openInNewWindow = window.event.ctrlKey;
+    }
+
 }
