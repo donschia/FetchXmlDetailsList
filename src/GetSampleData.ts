243938ad39eb7e726751fb0f8d8f52fed9c32436
Don Schiavone <oke20292@onegas.com>
2023-08-29 16:18:33 -0500

    Combined Fields added to documentation

--- src/GetSampleData.ts
+++ src/GetSampleData.ts
@@ -1,11 +1,18 @@
+// Use these two lines for Sample Contract dataset
 //import * as sampleResponseData from './data/sample.Contracts.Response.webapi.json';
 //import * as sampleResponseColumnLayout from './data/sample.Contracts.columnLayout.webapi.json';
+
+// Use these two lines for Sample Connections dataset
 import * as sampleResponseData from './data/sample.Connections.Response.webapi.json';
 import * as sampleResponseColumnLayout from './data/sample.Connections.columnLayout.webapi.json';
 
 export function GetSampleData() {
     // For WebApi response with value node
+
+    // Use following for Sample Contract dataset
     // return { dataItems : sampleResponseData.value, columns : sampleResponseColumnLayout, primaryEntityName : 'account' };
+    
+    // Use following for Sample Connections dataset
     return { dataItems : sampleResponseData.value, columns : sampleResponseColumnLayout, primaryEntityName : 'connection' };
 
     // For dynamics-web-api response without value node
